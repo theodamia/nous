8ed3dae08aaf1c147740b355c5125283ebd43a0b
teo <theodamia@gmail.com>
2026-01-08 13:08:10 +0200

    Removed default header component

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,30 +2,33 @@ import { TanStackDevtools } from '@tanstack/react-devtools';
 import type { QueryClient } from '@tanstack/react-query';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-import Header from '../components/header';
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
+import usePreventScrollLock from '../hooks/use-prevent-scroll-lock';
 
 interface MyRouterContext {
   queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  component: () => (
-    <>
-      <Header />
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
-    </>
-  ),
+  component: () => {
+    usePreventScrollLock();
+
+    return (
+      <>
+        <Outlet />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      </>
+    );
+  },
 });

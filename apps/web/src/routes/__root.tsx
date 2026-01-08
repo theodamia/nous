694ed0464037f6d8bbf57af56c7de32ae3a33364
teo <theodamia@gmail.com>
2026-01-08 13:29:22 +0200

    Formating changes

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,8 +2,8 @@ import { TanStackDevtools } from '@tanstack/react-devtools';
 import type { QueryClient } from '@tanstack/react-query';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 import usePreventScrollLock from '../hooks/use-prevent-scroll-lock';
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 
 interface MyRouterContext {
   queryClient: QueryClient;
